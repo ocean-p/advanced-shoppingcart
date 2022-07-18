3d42d684d9ecacfa357c1563db100fad01d5758a
ocean-p <duongkodoiqua@gmail.com>
2022-07-18 13:44:13 +0700

    routing and style Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import About from './pages/About';
+import Store from './pages/Store';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/store' element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
